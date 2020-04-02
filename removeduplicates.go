6626dc79de36534c537f9e5fdfe0414465c981fb
生虎 <shenghuli@users.noreply.github.com>
2020-04-02 11:48:05 +0800

    Update removeduplicates.go

--- removeduplicates.go
+++ removeduplicates.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	//删除排序数组中的重复项
 	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	fmt.Println(removeduplicates.RemoveDuplicates(nums))
+	fmt.Println(RemoveDuplicates(nums))
 	fmt.Println(nums)
 }
 
